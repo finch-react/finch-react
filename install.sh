5860a43dbdf6682ede602245f81903bae83532e7
Mikhail Kraynov <kraynov@finch-melrose.com>
2016-01-27 13:38:23 +0300

    finch-react-example -> finch-react-dev

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ set -e
 cd finch-react-styles && npm i && cd -
 cd finch-react-web && npm i && cd -
 cd finch-react-server && npm i && cd -
-cd finch-react-example && npm i && cd -
+cd finch-react-dev && npm i && cd -
