ea474ea81b70c4ce83f82980ef268f821545f215
moshkin <moshkin@finch-melrose.com>
2017-01-20 11:42:38 +0300

    link and tabbar

--- packages/finch-react-dev/app/src/components/Link.js
+++ packages/finch-react-dev/app/src/components/Link.js
@@ -1,24 +1,28 @@
 import React from 'react-native';
 let { PropTypes } = React;
 import FinchReactCore from 'finch-react-core';
-let { StyledComponent, Location } = FinchReactCore;
+let { StyledComponentNew, Location } = FinchReactCore;
 
-export default class Link extends StyledComponent {
+export default class Link extends StyledComponentNew {
+
+  styles = require('./Link.css');
+
   static propTypes = {
     href: PropTypes.string
   };
 
+  onPress = (e) => {
+    e.preventDefault();
+    Location.push(this.props.href);
+    console.log(`Web Link pressed ${this.props.href}`);
+  };
+
   render() {
     return (
-      <a href={`${this.props.href}?nojs`} onClick={this.onPress.bind(this)}>
+      <a href={`${this.props.href}?nojs`} onClick={this.onPress}>
         {this.props.children}
       </a>
     );
   }
 
-  onPress(e) {
-    e.preventDefault();
-    Location.push(this.props.href);
-    console.log(`Web Link pressed ${this.props.href}`);
-  }
 }
--- packages/finch-react-dev/app/src/components/TabBar.js
+++ packages/finch-react-dev/app/src/components/TabBar.js
@@ -1,39 +1,28 @@
 import React, {
   PropTypes,
-  View,
-  TouchableHighlight,
 } from 'react-native';
 import FinchReactCore from 'finch-react-core';
-let { StyledComponent } = FinchReactCore;
+let { StyledComponentNew } = FinchReactCore;
 import Link from '../components/Link';
 
-export default class TabBar extends StyledComponent {
+export default class TabBar extends StyledComponentNew {
+
+  styles = require('./TabBar.css');
+
   static propTypes = {
     title: PropTypes.string,
     items: PropTypes.any
   };
 
   render() {
+    const {main} = this.styles.locals;
     return (
-      <View>
+      <div className={main}>
         <Link element="link" href='/top'>Лучшее</Link>
         <Link element="link" href='/hot'>Горячее</Link>
         <Link element="link" href='/new'>Новое</Link>
-      </View>
+      </div>
     )
   }
 
-  static styles = {
-    main: {
-      flexDirection: 'row',
-      backgroundColor: '#ebe9e9',
-    },
-    link: {
-      flex: 1,
-      textAlign: 'center',
-      justifyContent: 'center',
-      paddingVertical: 10,
-      color: '#999'
-    }
-  };
 }
